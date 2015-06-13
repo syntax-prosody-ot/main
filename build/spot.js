e25804c1fb0921ed481654aa59b343d55e2dd47d
Ozan <ozangds@gmail.com>
2015-06-12 23:07:05 -0700

    nonrecursivity #7

--- build/spot.js
+++ build/spot.js
@@ -348,7 +348,7 @@ window.GEN = function(sTree, words, options){
 		leaves.push(omegafy(words[i]));
 	}
 	
-	var rootlessCand = addPhiWrapped(gen(leaves));
+	var rootlessCand = addPhiWrapped(gen(leaves, options), options);
 	
 	var candidates = [];
 	for(var i=0; i<rootlessCand.length; i++){
@@ -384,22 +384,25 @@ function gen(leaves, options){
 	var candidates = [];	//each candidate will be an array of siblings
 	if(!(leaves instanceof Array))
 		throw new Error(leaves+" is not a list of leaves.");	
-	
+
 	//Base case: 0 leaves
 	if(leaves.length === 0){
 		candidates.push([]);
 		return candidates;
 	}
-	
-	
-	
+
+
+
 	//Recursive case: at least 1 word. Consider all candidates where the first i words are grouped together
 	for(var i = 1; i <= leaves.length; i++){
-	
+
 		//Case 1: the first i leaves attach directly to parent (no phi wrapping)
-		
+	
 		var leftside = leaves.slice(0,i);
-		var rightsides = addPhiWrapped(gen(leaves.slice(i, leaves.length)));
+		var rightsides = addPhiWrapped(gen(leaves.slice(i, leaves.length), options), options);
+		
+		// for case 1, we don't need to check the left side for nonrecursivity, because it's all leaves
+		
 		//Combine the all-leaf leftside with all the possible rightsides that have a phi at their left edge (or are empty)
 		for(var j = 0; j<rightsides.length; j++){
 			if(!rightsides[j].length || rightsides[j][0].cat === 'phi')
@@ -408,108 +411,64 @@ function gen(leaves, options){
 				candidates.push(cand);
 			}
 		}
+	
 		
-		
+	
 		//Case 2: the first i words are wrapped in a phi
 		if(i<leaves.length){
-			var phiLeftsides = gen(leaves.slice(0,i));
+			var phiLeftsides = gen(leaves.slice(0,i), options);
 			for(var k = 0; k<phiLeftsides.length; k++)
 			{
+				var phiNode = phiify(phiLeftsides[k], options);
+				if (!phiNode)
+					continue;
+				var leftside = [phiNode];
+				
 				for(var j = 0; j<rightsides.length; j++)
 				{
-					var leftParent = phiify(phiLeftsides[k]);
-					cand = [leftParent].concat(rightsides[j]);
+					cand = leftside.concat(rightsides[j]);
 					candidates.push(cand);
 				}
 			} 
 		}
-		
-	}
 	
+	}
+
 	return candidates;
 }
 
-function phiify(candidate){
-	return {id: 'phi'+(phiNum++), cat: 'phi', children: candidate}
+function topLevelRecursive(candidate) {
+	for (var i = 0; i < candidate.length; i++) {
+		var topNode = candidate[i];
+		var topChildren = topNode.children || [];
+		for (var j = 0; j < topChildren.length; j++)
+			if (topNode.cat === topChildren[j].cat)
+				return true;
+	}
+	return false;
+}
+
+function phiify(candidate, options){
+	if (options && options.obeysNonrecursivity)
+		for (var i = 0; i < candidate.length; i++)
+			if (candidate[i].cat === 'phi')
+				return null;
+	return {id: 'phi'+(phiNum++), cat: 'phi', children: candidate};
 }
 
 //Takes a list of candidates and doubles it to root each of them in a phi
-function addPhiWrapped(candidates){
+function addPhiWrapped(candidates, options){
 	var origLen = candidates.length;
 	for(var i=0; i<origLen; i++){
-		if(candidates[i].length)
-			candidates.push([phiify(candidates[i])]);
-	}
-	return candidates;
-}
-
-
-
-
-/*
-* Versions of Gen that don't make objects, just strings with parentheses
-*/
-function GENparen(words){
-	return addPhiWrapped(genparen(words));
-}
-
-function genparen(words){
-	var candidates = [];
-	if(!(words instanceof Array))
-		throw new Error(words+" is not a list of words.");	
-	
-	//Base case: 0 words
-	if(words.length === 0){
-		candidates.push('');
-		return candidates;
-	}
-	
-	
-	
-	//Recursive case: at least 1 word. Consider all candidates where the first i words are grouped together
-	for(var i = 1; i <= words.length; i++){
-	
-		//Case 1: the first i words attach directly to iota (no phi wrapping)
-		
-		var leftside = words.slice(0,i).join(' ');
-		var rightsides = addPhiWrapped(genparen(words.slice(i, words.length)));
-		//Combine the non-phi-wrapped leftside with all the possible rightsides that have a phi at their left edge (or are empty)
-		for(var j = 0; j<rightsides.length; j++){
-			if(!rightsides[j] || rightsides[j][0] === '(')
-			{
-				var cand = leftside+rightsides[j];
-				candidates.push(cand);
-			}
+		if(candidates[i].length) {
+			var phiNode = phiify(candidates[i], options);
+			if (phiNode)
+				candidates.push([phiNode]);
 		}
-		
-		
-		//Case 2: the first i words are wrapped in a phi
-		if(i<words.length){
-			var phiLeftsides = genparen(words.slice(0,i));
-			for(var k = 0; k<phiLeftsides.length; k++)
-			{
-				for(var j = 0; j<rightsides.length; j++)
-				{
-					cand = '('+phiLeftsides[k]+')'+rightsides[j];
-					candidates.push(cand);
-				}
-			} 
-		}
-		
 	}
-	
 	return candidates;
 }
 
-//Takes a list of candidates and doubles it to root each of them in a phi
-function addPhiWrappedparen(candidates){
-	var origLen = candidates.length;
-	for(var i=0; i<origLen; i++){
-		if(candidates[i])
-			candidates.push('('+candidates[i]+')');
-	}
-	return candidates;
-}
 })();
 //An array of pairs to define which syntactic categories "match" which prosodic categories.
 //For theory comparison, we'll want one array for each theory.
