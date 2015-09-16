8b9ce5a19741475ef84e4ba757e1efe629921998
jbellik <jbellik@ucsc.edu>
2015-09-16 13:42:28 -0700

    Truckenbrodt-style non-recursivity implemented

--- build/constraints.js
+++ build/constraints.js
@@ -494,7 +494,7 @@ function nonRec1(s, parent, cat){
 		return 0;
 	}
 	
-	//Recursive case: if parent is non_terminal, find out how many violations are in each of the subtrees rooted in its children
+	//Recursive case: if parent is non-terminal, find out how many violations are in each of the subtrees rooted in its children
 	var vcount = 0;
 	var child;
 	
@@ -503,10 +503,50 @@ function nonRec1(s, parent, cat){
 		if (parent.cat===cat && child.cat===cat){
 			vcount++;
 		}		
-		vcount+=nonRec1(child, cat);
+		vcount+=nonRec1(s, child, cat);
 	}
 	return vcount;
 }
+
+
+/* Non-recursivity, Truckenbrodt style:
+*  "Any two p-phrases that are not disjoint in extension are identical in extension."
+*  For every node x of category p dominated by another node y of category p, 
+*  assign a violation for every leaf dominated by y that is not also dominated by x.
+*/
+function nonRecTruckenbrodt(s, parent, cat){
+	console.log("looking for nonRecTruckenbrodt violations in prosodic tree "+parent.id);
+	if(!parent.children||(parent.children.length===0)){
+		return 0;
+	}
+	
+	var vcount=0;
+	var child;
+	
+	for(var i=0; i<parent.children.length; i++){
+		child = parent.children[i];
+		if(parent.cat===cat && child.cat===cat){
+			vcount+=leafDifferenceSize(getLeaves(child), getLeaves(parent));
+		}
+		vcount+=nonRecTruckenbrodt(s, child, cat);
+	}
+	return vcount;
+}
+
+/*Given arrays x, y, where the elements in x are a subset of the elements in y,
+* and the elements in x and y are in the same order, returns the number of elements
+* in y that are not also in x.
+* TODO Modify this so that it doesn't make all the assumptions above concerning the relationship of x and y.
+*/
+function leafDifferenceSize(x,y){
+	if(!(x instanceof Array) || !(y instanceof Array)){
+		console.log("x: "+x);
+		console.log("y: "+y);
+		throw new Error("Either x or y is not an array");
+	}
+	return y.length-x.length;
+}
+	
 /* Assign a violation for every node whose leftmost daughter constituent is of type k
 *  and is lower in the prosodic hierarchy than its sister constituent immediately to its right: *(Kn Kn-1)
 *  Elfner's StrongStart.
