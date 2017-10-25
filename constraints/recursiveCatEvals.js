b3bcc0571419d9091216b2dba24998fdcfe883e8
jbellik <jbellik@users.noreply.github.com>
2017-10-25 13:20:26 -0700

    first try at marking min./max.

--- constraints/recursiveCatEvals.js
+++ constraints/recursiveCatEvals.js
@@ -1,6 +1,9 @@
 
 /*
 Returns true if node does not dominate any other nodes of its category
+Assumes all nodes have valid and relevant categories 
+(i.e., this is designed for prosodic trees and won't give the desired results
+if run on a syntactic tree that contains, e.g., bar levels).
 */
 function isMinimal(node){
 	var cat = node.cat;
@@ -28,4 +31,52 @@ function isMaximal(parent, child){
 	if(parent.cat===child.cat)
 		return false;
 	else return true;
+}
+
+/* Function that takes a tree and the category of its root's parent node
+	and labels all the nodes in the tree as being minimal or maximal 
+	instance of whatever category k they are, where:
+	minimal = does not dominate any nodes of category k
+	maximal = is not dominated by any nodes of category k
+	and level ordering is assumed (a node of category level k 
+	will never be dominated by a node of category < k).
+	
+	Previous isMin or isMax labels are preserved.
+*/
+// Move this to the prosodic hierarchy file probably?
+var sCat = ["cp", "xp", "x0"];
+
+function markMinMax(mytree, parcat){
+	// Check for maximality
+	if(!mytree.hasOwnProperty(isMax)){
+		mytree.isMax = (mytree.cat !== parcat)
+	}
+	
+	// Check for minimality
+	if(!mytree.hasOwnProperty(isMin){
+		mytree.isMin = isMinimal(mytree);
+		
+/* 		// Breadth-first search of the children to see if 
+		// any immediate children are the same category as the current node
+		var i = 0;
+		while(mytree.isMin; i < mytree.children.length){
+			mychild = mytree.children[i];
+			if(pCat.indexOf(mychild.cat) >= 0 || sCat.indexOf(mychild.cat) >= 0){
+				childcat = mychild.cat;
+				if(mytree.cat == childcat){
+					mytree.isMin = false;
+				}
+				else{
+					i++;
+				}
+				
+			} */		
+	}
+	if(mytree.children && mytree.children.length){
+		parcat = mytree.cat;
+		for(var i = 0; i < mytree.children.length; i++){
+			mytree.children[i] = markMinMax(mytree.children[i], parcat);
+		}
+	}
+	return mytree;
 }
