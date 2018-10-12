aeb0e0f5ea98fbed1d38bd3210d71e697268c1ce
Nick Kalivoda <nkalivoda@users.noreply.github.com>
2018-10-12 08:51:09 -0700

    Adding tree file for BSP tutorial

---
+++ trees/BSP_trees.js
@@ -0,0 +1,281 @@
+var bsp_Tree1 = {
+    "id": "TP",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "NP",
+            "children": [
+                {
+                    "id": "students",
+                    "cat": "x0"
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "VP",
+            "children": [
+                {
+                    "id": "read",
+                    "cat": "x0"
+                }
+            ]
+        }
+    ]
+};
+
+var bsp_Tree2 = {
+    "id": "TP",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "NP",
+            "children": [
+                {
+                    "cat": "xp",
+                    "id": "AP",
+                    "children": [
+                        {
+                            "id": "good",
+                            "cat": "x0"
+                        }
+                    ]
+                },
+                {
+                    "cat": "xp",
+                    "id": "NP",
+                    "children": [
+                        {
+                            "id": "students",
+                            "cat": "x0"
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "VP",
+            "children": [
+                {
+                    "id": "read",
+                    "cat": "x0"
+                }
+            ]
+        }
+    ]
+};
+
+var bsp_Tree3 = {
+    "id": "TP",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "NP1",
+            "children": [
+                {
+                    "id": "students",
+                    "cat": "x0"
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "VP",
+            "children": [
+                {
+                    "id": "read",
+                    "cat": "x0"
+                },
+                {
+                    "cat": "xp",
+                    "id": "NP2",
+                    "children": [
+                        {
+                            "id": "books",
+                            "cat": "x0"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
+
+var bsp_Tree4 = {
+    "id": "TP",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "NP1",
+            "children": [
+                {
+                    "cat": "xp",
+                    "id": "AP",
+                    "children": [
+                        {
+                            "id": "good",
+                            "cat": "x0"
+                        }
+                    ]
+                },
+                {
+                    "cat": "xp",
+                    "id": "NP2",
+                    "children": [
+                        {
+                            "id": "students",
+                            "cat": "x0"
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "VP",
+            "children": [
+                {
+                    "id": "read",
+                    "cat": "x0"
+                },
+                {
+                    "cat": "xp",
+                    "id": "NP3",
+                    "children": [
+                        {
+                            "id": "books",
+                            "cat": "x0"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
+
+var bsp_Tree5 = {
+    "id": "TP",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "NP3",
+            "children": [
+                {
+                    "id": "students",
+                    "cat": "x0"
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "VP",
+            "children": [
+                {
+                    "id": "read",
+                    "cat": "x0"
+                },
+                {
+                    "cat": "xp",
+                    "id": "NP2",
+                    "children": [
+                        {
+                            "cat": "xp",
+                            "id": "AP",
+                            "children": [
+                                {
+                                    "id": "long",
+                                    "cat": "x0"
+                                }
+                            ]
+                        },
+                        {
+                            "cat": "xp",
+                            "id": "NP1",
+                            "children": [
+                                {
+                                    "id": "books",
+                                    "cat": "x0"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
+
+var bsp_Tree6 = {
+    "id": "TP",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "NP1",
+            "children": [
+                {
+                    "cat": "xp",
+                    "id": "AP1",
+                    "children": [
+                        {
+                            "id": "good",
+                            "cat": "x0"
+                        }
+                    ]
+                },
+                {
+                    "cat": "xp",
+                    "id": "NP2",
+                    "children": [
+                        {
+                            "id": "students",
+                            "cat": "x0"
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "VP",
+            "children": [
+                {
+                    "id": "read",
+                    "cat": "x0"
+                },
+                {
+                    "cat": "xp",
+                    "id": "NP3",
+                    "children": [
+                        {
+                            "cat": "xp",
+                            "id": "AP2",
+                            "children": [
+                                {
+                                    "id": "long",
+                                    "cat": "x0"
+                                }
+                            ]
+                        },
+                        {
+                            "cat": "xp",
+                            "id": "NP4",
+                            "children": [
+                                {
+                                    "id": "books",
+                                    "cat": "x0"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
