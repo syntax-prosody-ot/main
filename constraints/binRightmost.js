1b4e535c09f0df1f06f103e5ba9d852d4e89877c
sgillespie931 <45107444+sgillespie931@users.noreply.github.com>
2019-04-15 21:12:34 -0700

    Create binRightmost.js
    Syntax is broken

---
+++ constraints/binRightmost.js
@@ -0,0 +1,23 @@
+function binaryRightmost(stree, ptree, cat)
+{
+var vcount=0;
+var root = ptree;
+if (root.cat = cat && numChil>2)
+{
+var chil+root.children;
+var mchil = chil.length;
+var head  = chi[n chil-1];
+vcount + (head.children.length != 2);
+}
+return vcount;
+};
+
+function binarityRightMost (S, ptree, cat) {
+var vcount = o;
+var root = ptree;
+if (root.cat = cat) {
+
+
+}
+
+}
