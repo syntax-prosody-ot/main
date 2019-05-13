899b9c6aa6ae122e2931c0a46a67e83e0d302e27
jbellik <jbellik@users.noreply.github.com>
2019-05-13 14:26:27 -0700

    adds comments to binMaxRight, changes constraint names + test appropriately

--- constraints/binRightmost.js
+++ constraints/binRightmost.js
@@ -1,6 +1,13 @@
-function binMaxRight (s, ptree, cat) {
+/* Assign a violation for every node of category cat 
+such that its rightmost child of category (cat-1) 
+has more than two children.
+*/
+
+function binMaxRightParent(s, ptree, cat) {
   var vcount = 0;
   if (ptree.children && ptree.children.length) {
+	  //This condition needs to change to be general across categories -- don't just check for 'phi'
+	  // You'll need to look at prosodichierarchy.js to help you out here.
     if (cat == 'phi') {
       var len = ptree.children.length - 1;
       var rightMost = ptree.children[len];
@@ -10,4 +17,13 @@ function binMaxRight (s, ptree, cat) {
     }
   }
   return vcount;
+}
+
+/* Assign a violation for every node x of category cat
+such that x is the rightmost child of its parent
+and x has more than 2 children.
+*/
+function binMaxRightChild(s, ptree, cat) {
+	var vcount = 0;
+	return vcount;
 }
