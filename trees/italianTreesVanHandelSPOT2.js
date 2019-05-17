f2e901d6eb74e7062027318817feff225f19475f
jbellik <jbellik@users.noreply.github.com>
2019-05-17 15:43:31 -0700

    Italian analysis files for Nick Van Handel

---
+++ trees/italianTreesVanHandelSPOT2.js
@@ -0,0 +1,45 @@
+
+var it_daro_libri = {
+    "id": "CP1",
+    "cat": "cp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "TP",
+            "children": [
+                {
+                    "id": "daro",
+                    "cat": "x0"
+                },
+                {
+                    "cat": "funcp",
+                    "id": "VP",
+                    "children": [
+                        {
+                            "cat": "xp",
+                            "id": "NP",
+                            "children": [
+                                {
+                                    "id": "libri",
+                                    "cat": "x0"
+                                }
+                            ]
+                        },
+                        {
+                            "cat": "xp",
+                            "id": "PP",
+                            "children": [
+                                {
+                                    "id": "a-Gianni",
+                                    "cat": "x0"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
+
+var it_ho_mangiato = {}
