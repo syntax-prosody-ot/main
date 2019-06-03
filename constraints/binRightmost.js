13bc264d1506ccc47b5c1ffcce937532da19f047
nnnguyenhoa <nnnguyenhoa@yahoo.com>
2019-06-02 20:22:40 -0700

    finished(?) binRightmost

--- constraints/binRightmost.js
+++ constraints/binRightmost.js
@@ -5,11 +5,9 @@ has more than two children.
 
 function binMaxRightParent(s, ptree, cat) {
   var vcount = 0;
-  if (ptree.children && ptree.children.length) {
+  if (ptree && ptree.children.length) {
     if (ptree.cat === cat) {
-      var len = ptree.children.length - 1;
-      var rightMost = ptree.children[len];
-      if (rightMost.children.length > 2) {
+      if (ptree.children.length > 2) {
         vcount++;
       }
     }
@@ -23,5 +21,21 @@ and x has more than 2 children.
 */
 function binMaxRightChild(s, ptree, cat) {
 	var vcount = 0;
-	return vcount;
+  if (ptree.children && ptree.children.length) {
+    //console.log(ptree.children[1].cat);
+    //console.log(cat);
+    var len = ptree.children.length - 1;
+    var rightMost = ptree.children[len];
+    var childLen = rightMost.children.length - 1;
+    var currChild = rightMost.children[childLen];
+    while(currChild.cat !== "w") {
+      rightMost = currChild;
+      childLen = rightMost.children.length - 1;
+      currChild = rightMost.children[childLen];
+    }
+    if(rightMost.children.length > 2) {
+      vcount++;
+    }
+  }
+  return vcount;
 }
