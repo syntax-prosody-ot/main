ebdc5f9c3d472f09b3bdd8abe2ee46b72dd9b71e
Jenny <jbellik@ucsc.edu>
2019-07-16 23:37:56 -0700

    creates trees and analysis file for Japanese accent analysis
    2,3,4 words; adds balanced trees

---
+++ trees/accent_trees_midbranching.js
@@ -0,0 +1,568 @@
+var uu_uu = {
+    "id": "XP1",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "XP_5",
+            "children": [
+                {
+                    "id": "u",
+                    "cat": "x0"
+                },
+                {
+                    "id": "u_1",
+                    "cat": "x0"
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "XP_6",
+            "children": [
+                {
+                    "id": "u_2",
+                    "cat": "x0"
+                },
+                {
+                    "id": "u_3",
+                    "cat": "x0"
+                }
+            ]
+        }
+    ]
+};
+
+var uu_ua = {
+    "id": "XP1",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "XP_5",
+            "children": [
+                {
+                    "id": "u",
+                    "cat": "x0"
+                },
+                {
+                    "id": "u_1",
+                    "cat": "x0"
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "XP_6",
+            "children": [
+                {
+                    "id": "u_2",
+                    "cat": "x0"
+                },
+                {
+                    "id": "a",
+                    "cat": "x0"
+                }
+            ]
+        }
+    ]
+};
+
+var uu_au = {
+    "id": "XP1",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "XP_5",
+            "children": [
+                {
+                    "id": "u",
+                    "cat": "x0"
+                },
+                {
+                    "id": "u_1",
+                    "cat": "x0"
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "XP_6",
+            "children": [
+                {
+                    "id": "a",
+                    "cat": "x0"
+                },
+                {
+                    "id": "u_2",
+                    "cat": "x0"
+                }
+            ]
+        }
+    ]
+};
+
+var uu_aa = {
+    "id": "XP1",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "XP_5",
+            "children": [
+                {
+                    "id": "u",
+                    "cat": "x0"
+                },
+                {
+                    "id": "u_1",
+                    "cat": "x0"
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "XP_6",
+            "children": [
+                {
+                    "id": "a",
+                    "cat": "x0"
+                },
+                {
+                    "id": "a_2",
+                    "cat": "x0"
+                }
+            ]
+        }
+    ]
+};
+
+var ua_uu = {
+    "id": "XP1",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "XP_5",
+            "children": [
+                {
+                    "id": "u",
+                    "cat": "x0"
+                },
+                {
+                    "id": "a",
+                    "cat": "x0"
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "XP_6",
+            "children": [
+                {
+                    "id": "u_1",
+                    "cat": "x0"
+                },
+                {
+                    "id": "u_2",
+                    "cat": "x0"
+                }
+            ]
+        }
+    ]
+};
+
+var ua_ua = {
+    "id": "XP1",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "XP_5",
+            "children": [
+                {
+                    "id": "u",
+                    "cat": "x0"
+                },
+                {
+                    "id": "a",
+                    "cat": "x0"
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "XP_6",
+            "children": [
+                {
+                    "id": "u_1",
+                    "cat": "x0"
+                },
+                {
+                    "id": "a_2",
+                    "cat": "x0"
+                }
+            ]
+        }
+    ]
+};
+
+var ua_au = {
+    "id": "XP1",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "XP_5",
+            "children": [
+                {
+                    "id": "u",
+                    "cat": "x0"
+                },
+                {
+                    "id": "a",
+                    "cat": "x0"
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "XP_6",
+            "children": [
+                {
+                    "id": "a_1",
+                    "cat": "x0"
+                },
+                {
+                    "id": "u_2",
+                    "cat": "x0"
+                }
+            ]
+        }
+    ]
+};
+
+var ua_aa = {
+    "id": "XP1",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "XP_5",
+            "children": [
+                {
+                    "id": "u",
+                    "cat": "x0"
+                },
+                {
+                    "id": "a",
+                    "cat": "x0"
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "XP_6",
+            "children": [
+                {
+                    "id": "a_1",
+                    "cat": "x0"
+                },
+                {
+                    "id": "a_2",
+                    "cat": "x0"
+                }
+            ]
+        }
+    ]
+};
+
+var au_uu = {
+    "id": "XP1",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "XP_5",
+            "children": [
+                {
+                    "id": "a",
+                    "cat": "x0"
+                },
+                {
+                    "id": "u",
+                    "cat": "x0"
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "XP_6",
+            "children": [
+                {
+                    "id": "u_1",
+                    "cat": "x0"
+                },
+                {
+                    "id": "u_2",
+                    "cat": "x0"
+                }
+            ]
+        }
+    ]
+};
+
+var au_ua = {
+    "id": "XP1",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "XP_5",
+            "children": [
+                {
+                    "id": "a",
+                    "cat": "x0"
+                },
+                {
+                    "id": "u",
+                    "cat": "x0"
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "XP_6",
+            "children": [
+                {
+                    "id": "u_1",
+                    "cat": "x0"
+                },
+                {
+                    "id": "a_2",
+                    "cat": "x0"
+                }
+            ]
+        }
+    ]
+};
+
+var au_au = {
+    "id": "XP1",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "XP_5",
+            "children": [
+                {
+                    "id": "a",
+                    "cat": "x0"
+                },
+                {
+                    "id": "u",
+                    "cat": "x0"
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "XP_6",
+            "children": [
+                {
+                    "id": "a_1",
+                    "cat": "x0"
+                },
+                {
+                    "id": "u_2",
+                    "cat": "x0"
+                }
+            ]
+        }
+    ]
+};
+
+var au_aa = {
+    "id": "XP1",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "XP_5",
+            "children": [
+                {
+                    "id": "a",
+                    "cat": "x0"
+                },
+                {
+                    "id": "u",
+                    "cat": "x0"
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "XP_6",
+            "children": [
+                {
+                    "id": "a_1",
+                    "cat": "x0"
+                },
+                {
+                    "id": "a_2",
+                    "cat": "x0"
+                }
+            ]
+        }
+    ]
+};
+
+var aa_uu = {
+    "id": "XP1",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "XP_5",
+            "children": [
+                {
+                    "id": "a",
+                    "cat": "x0"
+                },
+                {
+                    "id": "a_1",
+                    "cat": "x0"
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "XP_6",
+            "children": [
+                {
+                    "id": "u",
+                    "cat": "x0"
+                },
+                {
+                    "id": "u_1",
+                    "cat": "x0"
+                }
+            ]
+        }
+    ]
+};
+
+var aa_ua = {
+    "id": "XP1",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "XP_5",
+            "children": [
+                {
+                    "id": "a",
+                    "cat": "x0"
+                },
+                {
+                    "id": "a_1",
+                    "cat": "x0"
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "XP_6",
+            "children": [
+                {
+                    "id": "u",
+                    "cat": "x0"
+                },
+                {
+                    "id": "a_2",
+                    "cat": "x0"
+                }
+            ]
+        }
+    ]
+};
+
+var aa_au = {
+    "id": "XP1",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "XP_5",
+            "children": [
+                {
+                    "id": "a",
+                    "cat": "x0"
+                },
+                {
+                    "id": "a_1",
+                    "cat": "x0"
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "XP_6",
+            "children": [
+                {
+                    "id": "a_2",
+                    "cat": "x0"
+                },
+                {
+                    "id": "u",
+                    "cat": "x0"
+                }
+            ]
+        }
+    ]
+};
+
+var aa_aa = {
+    "id": "XP1",
+    "cat": "xp",
+    "children": [
+        {
+            "cat": "xp",
+            "id": "XP_5",
+            "children": [
+                {
+                    "id": "a",
+                    "cat": "x0"
+                },
+                {
+                    "id": "a_1",
+                    "cat": "x0"
+                }
+            ]
+        },
+        {
+            "cat": "xp",
+            "id": "XP_6",
+            "children": [
+                {
+                    "id": "a_2",
+                    "cat": "x0"
+                },
+                {
+                    "id": "a_3",
+                    "cat": "x0"
+                }
+            ]
+        }
+    ]
+};
+
+var balanced_accent_tree_names = ["uu_uu", "uu_ua", "uu_au", "uu_aa", "ua_uu", "ua_ua", "ua_au", "ua_aa", "au_uu", "au_ua", "au_au", "au_aa", "aa_uu", "aa_ua", "aa_au", "aa_aa"];
+
+var balanced_accent_tree_list = [];
+for(var k = 0; k < balanced_accent_tree_names.length; k++){
+    var name = balanced_accent_tree_names[k];
+    var tree = window[name];
+	balanced_accent_tree_list.push(tree);
+}
