a8dadb43bfbcc1c0ba4e963f3c2078ef00b42795
nnnguyenhoa <nnnguyenhoa@yahoo.com>
2019-07-28 10:01:18 -0700

    fixed the binRightmost issues I understood

--- constraints/binRightmost.js
+++ constraints/binRightmost.js
@@ -23,38 +23,56 @@ function binMaxRightmostBranches(s, ptree, cat) {
   }
   return vcount;
 };
-var parent_ptree;
+
 function binMaxRightmostLeaves(s, ptree, cat) {
-  //first find the first level of with cat --> recursive look into the children
+  //make parent_ptree static variable to keep track of the parent ptree
+  if(typeof parent_ptree == 'undefined') {
+    parent_ptree = null;
+  }
   var vcount = 0;
+  //if curr ptree has children
   if(ptree.children && ptree.children.length) {
-  if(ptree.cat === cat) {
-    var recur_flag = 0;
+    //and is the same cat as input cat
+    if(ptree.cat === cat) {
+      //set the recursion flag to 0
+      var recur_flag = 0;
+      //for each child in the curr ptree
+      for(var i = 0; i < ptree.children.length; i++) {
+	//check if it's cat is the same as the cat of the input
+        if(ptree.children[i].cat === cat /* && ptree.children[i].length > 2*/) {
+          //if it is, increment vcount because you can't have more than one of each cat
+          vcount++;
+	  //set recursion flag to 1
+	  recur_flag = 1;
+          //stop looking to prevent extra vcount incrementation
+	  break;
+        }
+      }
+      
+      //if the recursion flag never set off
+      if(recur_flag === 0) {
+	//then check if there are more than two children
+        if(ptree.children.length > 2) {
+          //if there is a parent tree aka when the cat is not iota, make sure it is the rightmost children of parent
+      	  if(parent_ptree && ptree === parent_ptree.children[parent_ptree.children.length-1]) {
+	    //increment if conditions are met
+            vcount++;
+      	  }
+	  //if parent_ptree is null aka we are in iota and the cat of ptree is the same as cat input
+	  //the second condition is set all the way at the top as this part is inclosed in that if statement
+	  if(parent_ptree === null) {
+            vcount++;
+          }
+        }
+      }
+    }  
+    //code to recursively look through tree
     for(var i = 0; i < ptree.children.length; i++) {
-      if(ptree.children[i].cat === cat) {
-        vcount++;
-	recur_flag = 1;
-	break;
-      }
+      //set parent_ptree to the current ptree
+      parent_ptree = ptree;
+      //recursively call on children of ptree
+      vcount += binMaxRightmostLeaves(s, ptree.children[i], cat);
     }
-    if(recur_flag === 0) {
-      if(ptree.children.length > 2) {
-      	if(parent_ptree && ptree === parent_ptree.children[parent_ptree.children.length-1]) {
-        	vcount++;
-      	}
-      }
-    }
-  }
-  for(var i = 0; i < ptree.children.length; i++) {
-  	parent_ptree = ptree;
-    vcount += binMaxRightmostLeaves(s, ptree.children[i], cat);
-  }
-  //once found save this level to look at other children for same cat
-  //break out of recursive loop if cat is found
-  //find if this is the bottom-most level of cat
-  //assign a violation for every level that cat is not the bottomost cat
-  //once it is the bottommost level of cat, check if that cat has more than 2 children
-  //assign a violation if it more than 2
   }
   return vcount;
 };
