720e5f57ba9401fe6b6d625dc7aec0097c5fbb9e
Max Tarlov <mtarlov@ucsc.edu>
2019-08-11 22:11:13 -0700

    equalStrengthBase works
    equalStrengthBase looks like it is working now. I started making the left-edge constraints.
    
    Still to do:
    - test equalStrengthLeft constraints
    - create equalStrengthRight constraints
    - test equalStrengthRight constraints
    - maybe get rid of the function edgeIndex. I don't think it needs to be its own function, much less a global function.

--- constraints/eaqualStrength.js
+++ constraints/eaqualStrength.js
@@ -5,7 +5,6 @@
  */
 
 function equalStrengthBase(stree, ptree, cat, edgeName){
-  var edgeIndex; //the index of the target edge for a equal strength constraint
   var sTerminals = getLeaves(stree); //syntactic terminals
   var pTerminals = getLeaves(ptree); //prosodic terminals
 
@@ -19,13 +18,15 @@ function equalStrengthBase(stree, ptree, cat, edgeName){
     pTerminals[i].edges = void(0);
   }
 
-  if (edgeName == 'left'){edgeIndex = 0}; //left edge is children[0]
-  if (edgeName == 'right')(edgeIndex = -1); //right edge is children[-1]
+  equalStrengthHelper(stree, cat, edgeName);//call recursive helper for syntax
+  equalStrengthHelper(ptree, cat, edgeName);//call helper for prosody
 
-  equalStrengthHelper(stree, cat, edgeIndex);//call recursive helper for syntax
-  equalStrengthHelper(ptree, cat, edgeIndex);//call helper for prosody
+  return [getLeaves(stree, getLeaves(ptree))];
+}
 
-  return void(0);
+function edgeIndex(subtree, name){
+  if (name == "left"){return 0;}
+  if (name == "right"){return subtree.children.length - 1;}
 }
 
 /* equalStrengthBase is run once per call so that sTerminals and pTerminals can
@@ -34,15 +35,14 @@ function equalStrengthBase(stree, ptree, cat, edgeName){
  * the helper function minimizes code repitition by taking only one tree and
  * being called for both ptree and stree.
  */
-function equalStrengthHelper(tree, cat, edgeIndex){
-
+function equalStrengthHelper(tree, cat, edgeName){
+  var boundaries = 0;
+  recursiveStrengthHelper(tree, cat, boundaries);
   //recursiveStrengthHelper only goes through the left/right branches of tree
   //arguments given unique names to avoid scope problems
-  function recursiveStrengthHelper(partree, category, edge, boundaries){
+  function recursiveStrengthHelper(partree, category, boundaries){
     // argument boundaries keeps track of the number of edges a terminal is on
-    if (!boundaries){
-      var boundaries = 0;
-    }
+    var edge = edgeIndex(partree, edgeName);
     if (partree.cat === category){
       boundaries ++; //increment boundaries when partree is of the right cat
     }
@@ -51,17 +51,31 @@ function equalStrengthHelper(tree, cat, edgeIndex){
       partree.edges = boundaries; //assign partree.edges the value of boundaries
     }
     if (partree.children && partree.children.length){
-      recursiveStrengthHelper(partree.children[edge], category, edge, boundaries);
+      recursiveStrengthHelper(partree.children[edge], category, boundaries);
     }
   }
-
   if (tree.children && tree.children.length){
     for (var i = 0; i < tree.children.length; i ++){
-      equalStrengthHelper(tree.children[i], cat, edgeIndex); //recursive function call
+      equalStrengthHelper(tree.children[i], cat, edgeName); //recursive function call
     }
   }
+}
 
-  return recursiveStrengthHelper(tree, cat, edgeIndex);
+function equalStrengthLeftSP(stree, ptree, cat){
+  var vcount = 0;
+  var terminals = equalStrengthBase(stree, ptree, cat, "left");
+  for (var i = 0; i < terminals[0].length; i ++){
+    if (terminals[0][i].edges > terminals[1][i].edges){
+      vcount += terminals[0][i].edges - terminals[1][i].edges;
+    }
+  }
+  return vcount;
+}
 
-  //return tree;
+function equalStrengthLeftPS(stree, ptree, cat){
+  return equalStrengthLeftSP(ptree, stree, cat);
+}
+
+function equalStrengthLeft(stree, ptree, cat){
+  return equalStrengthLeftSP(stree, ptree, cat) + equalStrengthLeftPS(stree, ptree, cat);
 }
