6b36505ae49bced5187a7abcce8bfc2edd763731
Jenny <jbellik@ucsc.edu>
2019-09-05 11:51:15 -0700

    implements a test; fixes bug; removes the category check

--- constraints/balancedSisters.js
+++ constraints/balancedSisters.js
@@ -1,32 +1,31 @@
 function balancedSistersAdj(stree, ptree, cat){
+    console.log(ptree);
     var vcount = 0;
-    if (!ptree.children || ptree.children.length < 2){
+    if ((!ptree.children) || ptree.children.length < 2){
         return vcount;
     }
 
     else{
-        if(ptree.cat === cat){
-            for(var i = 0; i < ptree.children.length-1; i++){
-                var sister1 = ptree.children[i];
-                var sister2 = ptree.children[i+1];
-                //Make sure there is a defined children array for each sister under consideration
-                if(!sister1.children){
-                    sister1.children = [];
-                } 
-                if(!sister2.children){
-                    sister2.children = [];
-                }
-    
-                //Assign a violation if the sisters do not have the same number of children
-                if(sister1.children.length != sister2.children.length){
-                    vcount++;
-                }
-    
+        for(var i = 0; i < ptree.children.length-1; i++){
+            var sister1 = ptree.children[i];
+            var sister2 = ptree.children[i+1];
+            //Make sure there is a defined children array for each sister under consideration
+            if(!sister1.children){
+                sister1.children = [];
+            } 
+            if(!sister2.children){
+                sister2.children = [];
             }
+
+            //Assign a violation if the sisters do not have the same number of children
+            if(sister1.children.length != sister2.children.length){
+                vcount++;
+            }
+    
         }
         
         for(var j = 0; j<ptree.children.length; j++){
-            vcount += balancedSistersAdj(ptree.children[j]);
+            vcount += balancedSistersAdj(stree, ptree.children[j], cat);
         }
 
         return vcount;
