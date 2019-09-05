6b0de1688c407bb4126884bc20360ccc8fe0937d
Jenny <jbellik@ucsc.edu>
2019-09-05 15:38:13 -0700

    makes balancedSisters sensitive to parent category

--- constraints/balancedSisters.js
+++ constraints/balancedSisters.js
@@ -1,5 +1,6 @@
 
 /* Assign a violation for every pair of adjacent sisters 
+   with a parent of category cat
    that do not have the same number of children. 
 */
 function balancedSistersAdj(stree, ptree, cat){
@@ -9,24 +10,25 @@ function balancedSistersAdj(stree, ptree, cat){
     }
 
     else{
-        for(var i = 0; i < ptree.children.length-1; i++){
-            var sister1 = ptree.children[i];
-            var sister2 = ptree.children[i+1];
-            //Make sure there is a defined children array for each sister under consideration
-            if(!sister1.children){
-                sister1.children = [];
-            } 
-            if(!sister2.children){
-                sister2.children = [];
-            }
-
-            //Assign a violation if the sisters do not have the same number of children
-            if(sister1.children.length != sister2.children.length){
-                vcount++;
-            }
+        if(ptree.cat===cat){
+            for(var i = 0; i < ptree.children.length-1; i++){
+                var sister1 = ptree.children[i];
+                var sister2 = ptree.children[i+1];
+                //Make sure there is a defined children array for each sister under consideration
+                if(!sister1.children){
+                    sister1.children = [];
+                } 
+                if(!sister2.children){
+                    sister2.children = [];
+                }
     
+                //Assign a violation if the sisters do not have the same number of children
+                if(sister1.children.length != sister2.children.length){
+                    vcount++;
+                }
+            }
         }
-        
+
         for(var j = 0; j<ptree.children.length; j++){
             vcount += balancedSistersAdj(stree, ptree.children[j], cat);
         }
@@ -35,8 +37,8 @@ function balancedSistersAdj(stree, ptree, cat){
     }
 }
 
-/* Assign a violation for every set of sisters that do not all have the same number of children.
-   Ignores category completely.
+/* Assign a violation for every set of sisters dominated by a node of category cat 
+   that do not all have the same number of children.
 */
 function balancedSisters(stree, ptree, cat){
     var vcount = 0;
@@ -47,28 +49,32 @@ function balancedSisters(stree, ptree, cat){
     }
 
     else{
-        // Base case: violation if the children have differing numbers of children
-        var imbalanceFound = false;
-        var i = 0;
-        while(!imbalanceFound && i < ptree.children.length-1){
-            var sister1 = ptree.children[i];
-            var sister2 = ptree.children[i+1];
-            //Make sure there is a defined children array for each sister under consideration
-            if(!sister1.children){
-                sister1.children = [];
-            } 
-            if(!sister2.children){
-                sister2.children = [];
-            }
+        if(ptree.cat===cat){
+            
+            // Base case: violation if the children have differing numbers of children
+            var imbalanceFound = false;
+            var i = 0;
+            while(!imbalanceFound && i < ptree.children.length-1){
+                var sister1 = ptree.children[i];
+                var sister2 = ptree.children[i+1];
+                //Make sure there is a defined children array for each sister under consideration
+                if(!sister1.children){
+                    sister1.children = [];
+                } 
+                if(!sister2.children){
+                    sister2.children = [];
+                }
 
-            //Assign a violation if the sisters do not have the same number of children
-            if(sister1.children.length != sister2.children.length){
-                imbalanceFound = true;
+                //Assign a violation if the sisters do not have the same number of children
+                if(sister1.children.length != sister2.children.length){
+                    imbalanceFound = true;
+                }
+                i++;
             }
-            i++;
+            if(imbalanceFound){
+                vcount++;
+            }
         }
-        if(imbalanceFound)
-            vcount++;
 
         // Recurse for every subtree
         for(var j = 0; j<ptree.children.length; j++){
