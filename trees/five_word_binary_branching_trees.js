3cc6b4b2b7fab8a654cf337ef4236d53309bdf00
Jenny <jbellik@ucsc.edu>
2019-09-10 19:55:14 -0700

    binarity typology study update: more trees, more constraints

--- trees/five_word_binary_branching_trees.js
+++ trees/five_word_binary_branching_trees.js
@@ -352,3 +352,120 @@ var tree_5w_7 =
         }
 
 var five_word_abstract_trees = [tree_5w_1, tree_5w_2, tree_5w_3, tree_5w_4, tree_5w_5, tree_5w_6, tree_5w_7];
+
+var selected_six_word_trees = [
+    {
+        "id": "CP1",
+        "cat": "cp",
+        "children": [
+            {
+                "cat": "xp",
+                "id": "XP_11",
+                "children": [
+                    {
+                        "cat": "xp",
+                        "id": "XP_10",
+                        "children": [
+                            {
+                                "cat": "xp",
+                                "id": "XP_9",
+                                "children": [
+                                    {
+                                        "cat": "xp",
+                                        "id": "XP_8",
+                                        "children": [
+                                            {
+                                                "cat": "xp",
+                                                "id": "XP_7",
+                                                "children": [
+                                                    {
+                                                        "id": "a",
+                                                        "cat": "x0"
+                                                    },
+                                                    {
+                                                        "id": "b",
+                                                        "cat": "x0"
+                                                    }
+                                                ]
+                                            },
+                                            {
+                                                "id": "c",
+                                                "cat": "x0"
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        "id": "d",
+                                        "cat": "x0"
+                                    }
+                                ]
+                            },
+                            {
+                                "id": "e",
+                                "cat": "x0"
+                            }
+                        ]
+                    },
+                    {
+                        "id": "f",
+                        "cat": "x0"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "id": "CP1",
+        "cat": "cp",
+        "children": [
+            {
+                "cat": "xp",
+                "id": "XP_11",
+                "children": [
+                    {
+                        "id": "a",
+                        "cat": "x0"
+                    },
+                    {
+                        "cat": "xp",
+                        "id": "XP_8",
+                        "children": [
+                            {
+                                "id": "b",
+                                "cat": "x0"
+                            },
+                            {
+                                "id": "c",
+                                "cat": "x0"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "cat": "xp",
+                "id": "XP_10",
+                "children": [
+                    {
+                        "cat": "xp",
+                        "id": "XP_9",
+                        "children": [
+                            {
+                                "id": "d",
+                                "cat": "x0"
+                            },
+                            {
+                                "id": "e",
+                                "cat": "x0"
+                            }
+                        ]
+                    },
+                    {
+                        "id": "f",
+                        "cat": "x0"
+                    }
+                ]
+            }
+        ]
+    }
+];
