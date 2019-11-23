3e79a11c74fdf4ca14f8cef8d9c7c59c98051ac1
Colin Chen <colin.a.chen@gmail.com>
2019-11-22 16:42:31 -0800

    add gen warnings

--- interface1.js
+++ interface1.js
@@ -401,6 +401,7 @@ window.addEventListener('load', function(){
 				throw terminalCategoryError;
 			}
 		}
+		
 
 		var genTones = false; //true if tones are selected
 
@@ -410,9 +411,25 @@ window.addEventListener('load', function(){
 			genTones = spotForm.toneOptions.value;
 			//console.log(genOptions);
 		}
+		console.log(genOptions);
 		var csvSegs = [];
 		for (var i = 0; i < sTrees.length; i++) {
 			var sTree = sTrees[i];
+			//warn user about using more than six terminals
+			if(getLeaves(sTree).length >= 6){
+				if(!confirm("Inputs of more than six terminals may run slowly and even freeze your browser, depending on the selected GEN options. Do you wish to continue?")){
+					throw new Error("Tried to run gen with more than six terminals");
+				}
+			}
+
+			//warn user about clitic movement
+			if (genOptions['cliticMovement'] && (!genOptions['noUnary'] && getLeaves(sTree).length >= 5) 
+											 || (genOptions['noUnary'] && getLeaves(sTree).length >= 7)){
+				if(!confirm("You have selected GEN settings that allow clitic reordering, and included a sentence of (X) terminals. This GEN may yield more than 10K candidates. To reduce the number of candidates, consider enforcing non-recursivity, exhaustivity, and/or branchingness for intermediate prosodic nodes. Do you wish to proceed with these settings?")){
+					throw new Error("");
+				}
+			} 
+			
 			if (genOptions['cliticMovement']){
 				var candidateSet = GENwithCliticMovement(sTree, pString, genOptions);
 			}
