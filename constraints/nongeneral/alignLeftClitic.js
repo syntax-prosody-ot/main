0f9809f410bd9a32d4f50270c2614c37f06cbcda
Jenny <jbellik@ucsc.edu>
2019-12-04 11:31:56 -0800

    update alignLeftClitic to use warnings; add definition

--- constraints/nongeneral/alignLeftClitic.js
+++ constraints/nongeneral/alignLeftClitic.js
@@ -1,11 +1,17 @@
+/* For the specified lexical item(s), which are assumed to  be clitics (category is not checked),
+*  assign a violation for every terminal that intervenes between the left edge of the tree
+*  and the lexical item. 
+*/
+
 function alignLeftClitic(stree, ptree, clitic){
     if(ptree.cat !== "i" && ptree.cat !== 'iota'){
-        console.log("Warning: you are calling alignLeftClitic on a tree that is not rooted in i");
+        console.warn("You are calling alignLeftClitic on a tree that is not rooted in i");
     }
     var leaves = getLeaves(ptree);
     var cliticPos = leaves.findIndex(function(element){return element.id===clitic;});
     if(cliticPos < 0){
-        throw new Error("The specificed clitic "+clitic+" was not found in this tree");
+        console.warn("The specificed clitic "+clitic+" was not found in this tree");
+        cliticPos = 0;
     }
     return cliticPos;
 }
