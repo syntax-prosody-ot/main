f22d0c3aeb7572b63638b22d7328c41ad05efb59
ivpetkov <ivpetkov@ucsc.edu>
2020-03-09 15:17:53 -0700

    added alert for hyphens and underscores

--- interface1.js
+++ interface1.js
@@ -655,9 +655,15 @@ window.addEventListener('load', function(){
 			// console.log(JSON.parse(tree.toJSON()));
 			// console.log(JSON.parse(tree.toJSON())['cat']);
 			var checkTree = JSON.parse(tree.toJSON());
-			parseCats(checkTree);
+			parseCats(checkTree)
 			return (checkTree); // bit of a hack to get around replacer not being called recursively
 		}), null, 4);
+
+		sTree = spotForm.sTree.value
+		console.log(sTree)
+		if(sTree.includes('-') || sTree.includes('_')) {
+			alert('Do not use hyphens or underscores in category or id names in the tree builder.');
+		}
 
 		document.getElementById('doneMessage').style.display = 'inline-block';
 		spotForm.inputToGen.value = "";
