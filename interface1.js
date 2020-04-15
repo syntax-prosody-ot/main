03deb84e55cadf81e0147c464d975caa3f510515
jbellik <jbellik@users.noreply.github.com>
2020-04-15 10:25:55 -0700

    Revert "Update input-gen-options from  master"

--- interface1.js
+++ interface1.js
@@ -256,7 +256,6 @@ function getSTrees() {
 		sTrees = [sTrees];
 	}
 	return sTrees;
-
 }
 
 function danishTrees() {
@@ -394,15 +393,29 @@ window.addEventListener('load', function(){
 			}
 		}
 
-		//Get the input syntactic tree.
-		var sTrees;
-		try{
-			sTrees = getSTrees();
+		// Get the automatically generated syntactic trees
+		if(document.getElementById('inputOptions').style.display == 'block') {
+			var sTrees;
+			try{
+				sTrees = getAutoSTreeList();
+			}
+			catch(e){
+				console.error(e);
+				alert(e.message);
+				return;
+			}
 		}
-		catch(e){
-			console.error(e);
-			alert(e.message);
-			return;
+		else {
+			// Get the input syntactic tree from tree builder
+			var sTrees;
+			try{
+				sTrees = getSTrees();
+			}
+			catch(e){
+				console.error(e);
+				alert(e.message);
+				return;
+			}
 		}
 
 		//Get input to GEN.
@@ -570,7 +583,6 @@ window.addEventListener('load', function(){
 
 	});
 
-
 	/*
 	document.getElementById("japaneseTonesInfo").addEventListener("click", toneInfoBlock("japanese"));
 	document.getElementById("irishTonesInfo").addEventListener("click", toneInfoBlock("irish"));
@@ -582,6 +594,13 @@ window.addEventListener('load', function(){
 	//Open the tree making GUI
 	document.getElementById('goButton').addEventListener('click', function(){
 		document.getElementById('treeUI').style.display = 'block';
+		document.getElementById('goButton').style.backgroundColor = 'white';
+		document.getElementById('goButton').style.borderColor = '#3A5370';
+		if(document.getElementById('inputOptions').style.display == 'block') {
+			document.getElementById('inputOptions').style.display = 'none';
+			document.getElementById('inputButton').style.backgroundColor = '#d0d8e0';
+			document.getElementById('inputButton').style.borderColor = '#d0d8e0';
+		}
 	});
 
 	function refreshHtmlTree(treeIndex) {
@@ -601,7 +620,7 @@ window.addEventListener('load', function(){
 
 
 	//Set up the table...
-	document.getElementById('goButton').addEventListener('click', function(){
+	document.getElementById('buildButton').addEventListener('click', function(){
 		// Get the string of terminals
 		var terminalString = spotForm.inputToGen.value;
 		var terminalList = terminalString.trim().split(/\s+/);
@@ -611,7 +630,6 @@ window.addEventListener('load', function(){
 		showUTree(tree);
 		document.getElementById('doneMessage').style.display = 'none';
 	});
-
 
 	// automatically generate syntax button
 	document.getElementById('inputButton').addEventListener('click', function(){
@@ -766,7 +784,6 @@ window.addEventListener('load', function(){
 		htmlChunks.push('</tbody></table>');
 		return htmlChunks.join('');
 	}
-
 
 
 	// For testing only
