52516e5fe4e1ec260f9007906e70ff6c54963a50
Jenny <jbellik@ucsc.edu>
2020-04-15 16:47:53 -0700

    clean up

--- inputCandidateGenerator.js
+++ inputCandidateGenerator.js
@@ -37,7 +37,7 @@ function sTreeGEN(terminalString, options)
     options.rootCategory = options.rootCategory || 'xp';
 
     //Run GEN on the provided terminal string
-    var autoSTreePairs = goodGEN({}, terminalString, options);
+    var autoSTreePairs = GEN({}, terminalString, options);
     //Select just the generated trees
     var sTreeList = autoSTreePairs.map(x=>x[1]);
 
