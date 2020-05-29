878d4d4aef49bed113606b1299f09f8aa788aeb5
Max Tarlov <mtarlov@ucsc.edu>
2020-05-28 17:22:07 -0700

    Revert "tabs for spaces"
    This reverts commit f577d5cfcc348e91dbde15b4017c05fed70795c8.

--- candidategenerator.js
+++ candidategenerator.js
@@ -2,32 +2,32 @@
 
 window.GEN_impl = function(sTree, leaves, options) {
 
-  var recursiveOptions = {};
-  for (var k in options) {
-    if (options.hasOwnProperty(k) && k !== 'requireRecWrapper')
-      recursiveOptions[k] = options[k];
-  }
+	var recursiveOptions = {};
+	for (var k in options) {
+		if (options.hasOwnProperty(k) && k !== 'requireRecWrapper')
+			recursiveOptions[k] = options[k];
+	}
 
-  /* if rootCategory and recursiveCategory are the same, we don't want to call
-   * addRecCatWrapped because half of the candidates will have a root node with
-   * only one child, which will be of the same category, ie. {i {i (...) (...)}}
-   */
-  var rootlessCand = gen(leaves, recursiveOptions)
-  if(options.rootCategory !== options.recursiveCategory){
-    rootlessCand = addRecCatWrapped(gen(leaves, recursiveOptions), options);
-  }
+	/* if rootCategory and recursiveCategory are the same, we don't want to call
+	 * addRecCatWrapped because half of the candidates will have a root node with
+	 * only one child, which will be of the same category, ie. {i {i (...) (...)}}
+	 */
+	var rootlessCand = gen(leaves, recursiveOptions)
+	if(options.rootCategory !== options.recursiveCategory){
+		rootlessCand = addRecCatWrapped(gen(leaves, recursiveOptions), options);
+	}
 
-  var candidates = [];
-  for(var i=0; i<rootlessCand.length; i++){
-    var pRoot = wrapInRootCat(rootlessCand[i], options);
-    if (!pRoot)
-      continue;
-    if (options.obeysHeadedness && !obeysHeadedness(pRoot))
-      continue;
-    
-    candidates.push([sTree, pRoot]);
-  }
-  return candidates;
+	var candidates = [];
+	for(var i=0; i<rootlessCand.length; i++){
+		var pRoot = wrapInRootCat(rootlessCand[i], options);
+		if (!pRoot)
+			continue;
+		if (options.obeysHeadedness && !obeysHeadedness(pRoot))
+			continue;
+		
+		candidates.push([sTree, pRoot]);
+	}
+	return candidates;
 }
 
 /* Function to check if a tree obeys headedness. Each node must either be be
@@ -36,174 +36,174 @@ window.GEN_impl = function(sTree, leaves, options) {
  * function, basically a constraint.
  */
 function obeysHeadedness(tree){
-  //inner function
-  function nodeIsHeaded(node) {
-    /* Function to check if a node is headed. Relies on the prosodic hierarchy being
-     * properly defined. Returns true iff 
-     * a. one of the node's children is of the category directly below its own category *    on the prosodic hierarchy,
-     * b. one of the node's descendants is of the same category as the node
-     * c. the node is terminal.
-     */
-    var children = node.children;
-    //vacuously true if node is terminal
-    if (!children)
-      return true;
-    for (var i = 0; i < children.length; i++)
-      if (children[i].cat === pCat.nextLower(node.cat) 
-      || children[i].cat === node.cat){
-        return true;
-      }
-      return false;
-  }
+	//inner function
+	function nodeIsHeaded(node) {
+		/* Function to check if a node is headed. Relies on the prosodic hierarchy being
+		 * properly defined. Returns true iff 
+		 * a. one of the node's children is of the category directly below its own category *    on the prosodic hierarchy,
+		 * b. one of the node's descendants is of the same category as the node
+		 * c. the node is terminal.
+		 */
+		var children = node.children;
+		//vacuously true if node is terminal
+		if (!children)
+			return true;
+		for (var i = 0; i < children.length; i++)
+			if (children[i].cat === pCat.nextLower(node.cat) 
+			|| children[i].cat === node.cat){
+				return true;
+			}
+			return false;
+	}
 
-  //outer function
-  //first, check the parent node
-  if (!nodeIsHeaded(tree))
-    return false;
-  //return false if one of the children does not obey headedness
-  if (tree.children){
-    for (var x = 0; x<tree.children.length; x++){
-      if (!obeysHeadedness(tree.children[x])) //recursive function call
-        return false;
-    }
-  }
-  //if we get this far, the tree obeys headedness
-  return true;
+	//outer function
+	//first, check the parent node
+	if (!nodeIsHeaded(tree))
+		return false;
+	//return false if one of the children does not obey headedness
+	if (tree.children){
+		for (var x = 0; x<tree.children.length; x++){
+			if (!obeysHeadedness(tree.children[x])) //recursive function call
+				return false;
+		}
+	}
+	//if we get this far, the tree obeys headedness
+	return true;
 }
 
 function obeysExhaustivity(cat, children) {
-  for (var i = 0; i < children.length; i++)
-    if (cat !== children[i].cat && pCat.nextLower(cat) !== children[i].cat){
-      return false;
-    }
-  return true;
+	for (var i = 0; i < children.length; i++)
+		if (cat !== children[i].cat && pCat.nextLower(cat) !== children[i].cat){
+			return false;
+		}
+	return true;
 }
 
 function wrapInRootCat(candidate, options){
-  if (options && options.obeysExhaustivity){ // check that options.obeysExhaustivity is defined
-    if(typeof options.obeysExhaustivity ==="boolean" && options.obeysExhaustivity && !obeysExhaustivity(options.rootCategory, candidate)){
-      return null;
-    }
-    else if (options.obeysExhaustivity instanceof Array && options.obeysExhaustivity.indexOf(options.rootCategory)>=0 && !obeysExhaustivity(options.rootCategory, candidate)){
-      return null;
-    }
-  }
+	if (options && options.obeysExhaustivity){ // check that options.obeysExhaustivity is defined
+		if(typeof options.obeysExhaustivity ==="boolean" && options.obeysExhaustivity && !obeysExhaustivity(options.rootCategory, candidate)){
+			return null;
+		}
+		else if (options.obeysExhaustivity instanceof Array && options.obeysExhaustivity.indexOf(options.rootCategory)>=0 && !obeysExhaustivity(options.rootCategory, candidate)){
+			return null;
+		}
+	}
 
-  if(candidate.length > 2 && options.rootCategory === candidate[0].cat){
-    return null;
-  }
-  //if we get here, there aren't any relevant exhaustivity violations
-  return {id: 'root', cat: options.rootCategory, children: candidate};
+	if(candidate.length > 2 && options.rootCategory === candidate[0].cat){
+		return null;
+	}
+	//if we get here, there aren't any relevant exhaustivity violations
+	return {id: 'root', cat: options.rootCategory, children: candidate};
 }
 
 /*Conceptually, returns all possible parenthesizations of leaves that don't
-*  have a set of parentheses enclosing all of the leaves
+*	have a set of parentheses enclosing all of the leaves
 * Format: returns an array of parenthesizations, where each parenthesization
-*  is an array of children, where each child is
-*  either a node of category recursiveCategory (with descendant nodes attached) 
-*  or a leaf (of category terminalCategory)
+*	is an array of children, where each child is
+*	either a node of category recursiveCategory (with descendant nodes attached) 
+*	or a leaf (of category terminalCategory)
 * Options:
 */
 function gen(leaves, options){
-  
-  var candidates = [];  //each candidate will be an array of siblings
-  var cand;
-  if(!(leaves instanceof Array))
-    throw new Error(leaves+" is not a list of leaves.");
+	
+	var candidates = [];	//each candidate will be an array of siblings
+	var cand;
+	if(!(leaves instanceof Array))
+		throw new Error(leaves+" is not a list of leaves.");
 
-  //Base case: 0 leaves
-  if(leaves.length === 0){
-    candidates.push([]);
-    return candidates;
-  }
+	//Base case: 0 leaves
+	if(leaves.length === 0){
+		candidates.push([]);
+		return candidates;
+	}
 
-  //Recursive case: at least 1 terminal. Consider all candidates where the first i words are grouped together
-  for(var i = 1; i <= leaves.length; i++){
+	//Recursive case: at least 1 terminal. Consider all candidates where the first i words are grouped together
+	for(var i = 1; i <= leaves.length; i++){
 
-    //First, create the right sides:
-    var rightLeaves = leaves.slice(i, leaves.length);
-    
-    //recursion at top level
-    var rightsides = addRecCatWrapped(gen(rightLeaves, options), options);
-    //recursion at lower levels
-    if(pushRecCat(options)){
-      var wRightsides = addRecCatWrapped(gen(rightLeaves, options), options);
-      rightsides.concat(wRightsides);
-      popRecCat(options);
-    }
-    
-    
+		//First, create the right sides:
+		var rightLeaves = leaves.slice(i, leaves.length);
+		
+		//recursion at top level
+		var rightsides = addRecCatWrapped(gen(rightLeaves, options), options);
+		//recursion at lower levels
+		if(pushRecCat(options)){
+			var wRightsides = addRecCatWrapped(gen(rightLeaves, options), options);
+			rightsides.concat(wRightsides);
+			popRecCat(options);
+		}
+		
+		
 
-    //Then create left sides and combine them with the right sides.
+		//Then create left sides and combine them with the right sides.
 
-    //Case 1: the first i leaves attach directly to parent (no wrapping in a recursive category)
-    var leftside = leaves.slice(0,i);
+		//Case 1: the first i leaves attach directly to parent (no wrapping in a recursive category)
+		var leftside = leaves.slice(0,i);
 
-    // We don't need to check the left side for nonrecursivity, because it's all leaves
+		// We don't need to check the left side for nonrecursivity, because it's all leaves
 
-    //Combine the all-leaf leftside with all the possible rightsides that have a phi at their left edge (or are empty)
-    for(var j = 0; j<rightsides.length; j++){
-      var currRightside = rightsides[j];
-      var firstRight = currRightside[0];
-      if(!currRightside.length || (firstRight.children && firstRight.children.length) || (firstRight.cat != options.terminalCategory))
-      {
-        cand = leftside.concat(currRightside);
-        candidates.push(cand);
-      }
-    }
+		//Combine the all-leaf leftside with all the possible rightsides that have a phi at their left edge (or are empty)
+		for(var j = 0; j<rightsides.length; j++){
+			var currRightside = rightsides[j];
+			var firstRight = currRightside[0];
+			if(!currRightside.length || (firstRight.children && firstRight.children.length) || (firstRight.cat != options.terminalCategory))
+			{
+				cand = leftside.concat(currRightside);
+				candidates.push(cand);
+			}
+		}
 
-    
+		
 
 
-    
-    if(i<leaves.length){
-      if(options.noUnary && i<2){
-        continue;
-        //Don't generate any candidates where the first terminal is in an intermediate level node by itself.
-      }
+		
+		if(i<leaves.length){
+			if(options.noUnary && i<2){
+				continue;
+				//Don't generate any candidates where the first terminal is in an intermediate level node by itself.
+			}
 
-      //Case 2: the first i words are wrapped in an intermediate level node
-      //Case 2a: first recursive category
-      var phiLeftsides = gen(leaves.slice(0,i), options);
-      for(var k = 0; k<phiLeftsides.length; k++)
-      {
-        var phiNode = wrapInRecCat(phiLeftsides[k], options);
-        if (!phiNode){
-          continue;
-        }
-        var leftside = [phiNode];
+			//Case 2: the first i words are wrapped in an intermediate level node
+			//Case 2a: first recursive category
+			var phiLeftsides = gen(leaves.slice(0,i), options);
+			for(var k = 0; k<phiLeftsides.length; k++)
+			{
+				var phiNode = wrapInRecCat(phiLeftsides[k], options);
+				if (!phiNode){
+					continue;
+				}
+				var leftside = [phiNode];
 
-        for(var j = 0; j<rightsides.length; j++)
-        {
-          cand = leftside.concat(rightsides[j]);
-          candidates.push(cand);
-        }
-      }
+				for(var j = 0; j<rightsides.length; j++)
+				{
+					cand = leftside.concat(rightsides[j]);
+					candidates.push(cand);
+				}
+			}
 
-      //Case 3
-      //Try to build left-sides that are wrapped in the next lower recursive category but aren't wrapped in the current recursive category
-      if(pushRecCat(options)){
-        var wLeftsides = gen(leaves.slice(0,i), options);
-        for(var k = 0; k<wLeftsides.length; k++){
-          var wLeftside = wrapInRecCat(wLeftsides[k], options);
-          popRecCat(options);
-          if(wLeftside){
-            //console.log(i, "wLeftside:", wLeftside);
-            //Combine the all-leaf leftside with all the possible rightsides that aren't empty
-            for(var j = 0; j<rightsides.length; j++){
-              if(rightsides[j].length)
-              {
-                cand = [wLeftside].concat(rightsides[j]);
-                candidates.push(cand);
-              }
-            }
-          }
-        }  
-      }
+			//Case 3
+			//Try to build left-sides that are wrapped in the next lower recursive category but aren't wrapped in the current recursive category
+			if(pushRecCat(options)){
+				var wLeftsides = gen(leaves.slice(0,i), options);
+				for(var k = 0; k<wLeftsides.length; k++){
+					var wLeftside = wrapInRecCat(wLeftsides[k], options);
+					popRecCat(options);
+					if(wLeftside){
+						//console.log(i, "wLeftside:", wLeftside);
+						//Combine the all-leaf leftside with all the possible rightsides that aren't empty
+						for(var j = 0; j<rightsides.length; j++){
+							if(rightsides[j].length)
+							{
+								cand = [wLeftside].concat(rightsides[j]);
+								candidates.push(cand);
+							}
+						}
+					}
+				}	
+			}
  
-    }
+		}
 
-  }
+	}
 
 	//Now try to use recursion at the next recursive category
 	if (pushRecCat(options)) {
