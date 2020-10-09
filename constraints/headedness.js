eca79472b4f1ab3a1869b34facf0ef857499520a
Max Tarlov <mtarlov@ucsc.edu>
2020-10-09 15:01:03 -0700

    headedness constraint backend works, tested

--- constraints/headedness.js
+++ constraints/headedness.js
@@ -3,26 +3,40 @@
 function headedness(stree, ptree, cat){
     //code here
     //should return integer
-    function getsViolation(node, category) {
-        let result = true;
-        for (child of node.children) {
-            if(child.cat == category || node.cat == pCat.nextLower(node.cat)) {
-                result = false;
-                /*  methods to know for categories on the hierarchy
-                    pCat.isHigher
-                    pCat.isLower
-                    pCat.nextHigher
-                    pCat.nextLower
-                */
+    function getsViolation(node) {
+        if(ptree.children && ptree.children.length) {
+            let result = true;
+            for (let i = 0; i < node.children.length; i++) {
+                let child = node.children[i];
+                if(child.cat === node.cat || child.cat === pCat.nextLower(node.cat)) {
+                    result = false;
+                    /*  methods to know for categories on the hierarchy
+                        pCat.isHigher
+                        pCat.isLower
+                        pCat.nextHigher
+                        pCat.nextLower
+                    */
+                }
             }
+            return result;
         }
-        return result;
+        else {
+            return false;
+        }
+    }
+
+    function correctCategory(treeCat, constraintCat) {
+        if(constraintCat) {
+            return treeCat == constraintCat;
+        }
+        else {
+            return true;
+        }
     }
 
     let violations = 0;
-    cat = cat || true;
 
-    if(ptree.children && ptree.children.length && getsViolation(ptree, cat)) {
+    if(getsViolation(ptree) && correctCategory(ptree.cat, cat)) {
         violations ++;
     }
 
