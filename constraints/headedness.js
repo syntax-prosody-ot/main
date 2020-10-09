f397e61c1390cadb5e9e67494814d22b75284812
Max Tarlov <mtarlov@ucsc.edu>
2020-10-09 13:50:02 -0700

    headedness constraint preliminary

--- constraints/headedness.js
+++ constraints/headedness.js
@@ -3,4 +3,34 @@
 function headedness(stree, ptree, cat){
     //code here
     //should return integer
+    function getsViolation(node, category) {
+        let result = true;
+        for (child of node.children) {
+            if(child.cat == category || node.cat == pCat.nextLower(node.cat)) {
+                result = false;
+                /*  methods to know for categories on the hierarchy
+                    pCat.isHigher
+                    pCat.isLower
+                    pCat.nextHigher
+                    pCat.nextLower
+                */
+            }
+        }
+        return result;
+    }
+
+    let violations = 0;
+    let cat = cat || true;
+
+    if(ptree.children && ptree.children.length && getsViolation(ptree, cat)) {
+        violations ++;
+    }
+
+    if(ptree.children && ptree.children.length){
+        for(child of ptree){
+            violations += headedness(stree, child, cat);
+        }
+    }
+
+    return violations;
 }
