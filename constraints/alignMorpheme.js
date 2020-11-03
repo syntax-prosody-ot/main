cc5fcd9d7d84e546ca6fbd2952807e93a9bdad79
Jenny <jbellik@ucsc.edu>
2020-11-03 12:03:36 -0800

    generalize messages

--- constraints/alignMorpheme.js
+++ constraints/alignMorpheme.js
@@ -5,13 +5,13 @@
 
 function alignMorpheme(stree, ptree, clitic, direction){
 	if(ptree.cat !== "i" && ptree.cat !== 'iota'){
-				displayWarning("You are calling alignLeftClitic on a tree that is not rooted in i");
+				displayWarning("You are calling alignMorpheme on a tree that is not rooted in i");
     }
     clitic = clitic.split(';');
     var leaves = getLeaves(ptree);
     var cliticPos = leaves.findIndex(function(element){return clitic.indexOf(element.id) >= 0;});
     if(cliticPos < 0){
-				displayWarning("The specified clitic "+clitic+" was not found in this tree");
+				console.warn("alignMorpheme(): The specified morpheme "+clitic+" was not found in this tree");
         cliticPos = 0;
     }
     if (direction == "left"){
