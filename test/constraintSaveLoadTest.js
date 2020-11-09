22c6c684b0194082cfe0189afa537ea806d2d4a1
Max Tarlov <mtarlov@ucsc.edu>
2020-11-09 09:20:12 -0800

    save pitch accent test

---
+++ test/constraintSaveLoadTest.js
@@ -0,0 +1,27 @@
+//Interface Testing with Mocha and Chai
+
+function runConstraintTest(skipSetUp) {
+    if(!skipSetUp) {setUpInterfaceTest();}
+    describe("Pitch accent Save/Load/Clear", function() {
+        let fieldset = document.getElementById("pitch_accent_fieldset");
+        let constraints = fieldset.querySelectorAll('[name="constraints"]');
+        let values = [];
+        let savedConstraints;
+        it("Save", function() {
+            for(let constraint of constraints) {
+                constraint.click();
+                values.push(constraint.value);
+            }
+
+            let savedString = record_analysis();
+            savedConstraints = JSON.parse(savedString).myCon;
+            //object is more usefull than string later on
+
+            for(let value of values){
+                let regex = new RegExp(value);
+                assert(savedString.search(regex) > 0, value + " was not saved!");
+            }
+        });
+    });
+    mocha.run();
+}
