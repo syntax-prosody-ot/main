102062407bd542d14bc18269d26299ac141d94a9
Max Tarlov <mtarlov@ucsc.edu>
2020-11-09 17:52:11 -0800

    check save/load for category-sensative constraints

--- test/constraintSaveLoadTest.js
+++ test/constraintSaveLoadTest.js
@@ -2,35 +2,101 @@
 
 function runConstraintTest(skipSetUp) {
     if(!skipSetUp) {setUpInterfaceTest();}
-    let savedConstraints, constraints;
 
-    function constraintSaveTest(inputs) {
-        let values = [];
-        for(let input of inputs) {
-            input.click();
-            values.push(input.value);
+    function ensureChecked(element) {
+        /*takes an HTML DOM element,
+          sets to "checked" if checkbox and unchecked with click()
+          returns whether initially checked
+        */
+        let checked = true;
+        if(element.type && element.type === "checkbox" && !element.checked){
+            element.click();
+            checked = false;
+        }
+        assert(!(element.type && element.type === "checkbox" && !element.checked),
+            "".concat(element.name, ">>", element.value, " is not behaving properly"));
+        return checked;
+    }
+
+    function constraintSaveTest(constraints, categories = []) {
+
+        let defaultString = record_analysis();
+        let searchTerms = {}; //perhapse a class would be better, but there is only one instance
+        
+        searchTerms.content = {};
+
+        searchTerms.add = function(term) {
+            if(searchTerms.content[term]) {
+                searchTerms.content[term].expected ++;
+            }
+            else {
+                let initialMatches = [...defaultString.matchAll(new RegExp(term, 'g'))].length;
+                searchTerms.content[term] = {
+                    initial: initialMatches,
+                    expected: initialMatches + 1,
+                };
+            }
+        }
+
+        for(let constraint of constraints) {
+            ensureChecked(constraint);
+            searchTerms.add(constraint.value);
+        }
+
+        for(let category of categories) {
+            if(!ensureChecked(category)) {
+                //this assumes there is one and only one default category per constraint
+                searchTerms.add(category.name.split('-')[1]);
+            }
         }
 
         let savedString = record_analysis();
         let saveObject = JSON.parse(savedString);
         //object is more usefull than string later on
 
-        for(let value of values){
-            let regex = new RegExp(value);
-            assert(savedString.search(regex) > 0, value + " was not saved!");
+        for(let term of Object.keys(searchTerms.content)){
+            let regex = new RegExp(term, 'g');
+            assert([...savedString.matchAll(regex)].length === searchTerms.content[term].expected,
+                term + " was not saved!");
         }
 
         return saveObject;
     }
 
     describe("Layering and Structure Save/Load/Clear", function() {
-        //nothing yet
+        let savedConstraints, constraints, categories;
+        beforeEach(function() {
+            constraints = document.querySelectorAll(
+                '#layering_and_structure_fieldset [name="constraints"]');
+            categories = document.querySelectorAll(
+                '#layering_and_structure_fieldset [name^="category-"]');
+        });
+
+        it("Save", function() {
+            savedConstraints = constraintSaveTest(constraints, categories).myCon;
+            //assert(false, "This is to remind Max that this test should be failing right now");
+        });
+        
+        it("Clear", function () {
+            clearAnalysis();
+            for(let constraint of constraints){
+                assert(!constraint.checked, constraint.value + " was not cleared!");
+            }
+        });
+
+        it("Load", function () {
+            my_built_in_analysis({}, false, {}, savedConstraints);
+            for(let constraint of constraints) {
+                assert(constraint.checked, constraint.value + " did not load!");
+            }
+        });
     });
 
     describe("Pitch accent Save/Load/Clear", function() {
+        let savedConstraints, constraints;
         beforeEach(function() {
-            constraints = document.getElementById("pitch_accent_fieldset")
-                .querySelectorAll('[name="constraints"]');
+            constraints = document.querySelectorAll(
+                '#pitch_accent_fieldset [name="constraints"]');
         });
 
         it("Save", function() {
