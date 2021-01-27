d19439c6d79eebd26c861eda6da13ca86a7f3bef
Su Zin <szin@ucsc.edu>
2021-01-27 12:34:28 -0800

    testbuild

--- testBuild.sh
+++ testBuild.sh
@@ -1,3 +1,4 @@
 cat constraints/*.js > build/constraints.js
 cat build/constraints.js *.js test/*.js > build/test.js
-cat build/test.js lib/test/*.js > build/spot.js
+cat interface_JS/*.js > build/interface.js
+cat build/test.js lib/test/*.js build/interface.js > build/spot.js
