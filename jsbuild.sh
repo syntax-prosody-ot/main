e12057ad3d59e27886ea3cb340d3741b364dd6ba
Jenny <jbellik@ucsc.edu>
2021-02-18 12:04:42 -0800

    comments

--- jsbuild.sh
+++ jsbuild.sh
@@ -1,5 +1,6 @@
-cat constraints/*.js > build/constraints.js
-cat interface_JS/*.js > build/interface.js
-cat *.js > build/top.js
-cat build/constraints.js build/interface.js build/top.js > build/spot.js
+#basic build script for using the app
+cat constraints/*.js > build/constraints.js #constraints.js is all the constraints
+cat interface_JS/*.js > build/interface.js #interface.js is the code formerly in interface1.js
+cat *.js > build/top.js #top.js is the code in the top level of the main directory
+cat build/constraints.js build/interface.js build/top.js > build/spot.js #spot.js is everything put together
 
