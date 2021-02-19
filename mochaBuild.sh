ef78c999de9ec825b1afff6578289ea3c79e6a9d
Jenny <jbellik@ucsc.edu>
2021-02-18 21:27:54 -0800

    reorder

--- mochaBuild.sh
+++ mochaBuild.sh
@@ -1,3 +1,3 @@
 #build script to create a test file for mocha
 source testBuild.sh
-cat build/test.js build/top.js build/constraints.js > test/compiledTest/test.js #excludes interface.js since this runs from the console
+cat build/top.js build/constraints.js build/test.js > test/compiledTest/test.js #excludes interface.js since this runs from the console
