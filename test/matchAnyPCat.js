bd469fa73073c9c624073feb57a04210e3fc4afa
Su Zin <szin@ucsc.edu>
2021-04-22 09:34:47 -0700

    matchAnyPCat.html

--- test/matchAnyPCat.js
+++ test/matchAnyPCat.js
@@ -2,30 +2,153 @@ function matchAnyPCat(){
     var stree1 ={
         "id": "CP1",
         "cat": "cp",
-        "children": [ { "id": "a", "cat": "x0"},
-        {"cat": "cp", "id": "XP_4", "children": [ {"id": "b", "cat": "x0"}, { "id": "c", "cat": "x0"} ] } ]
-        };
-        
-       // CPs can be matched to phis
-       // ptree1 = (a (b c)) (where parentheses stand for the edges of nodes with cat: 'phi')
-       // ptree2 = ((a b) c)
-       // matchSP(stree1, ptree1, 'cp', {anyPCat:true}) = 0
-       // matchSP(stree1, ptree2, 'cp', {anyPCat:true}) = 1
+        "children": [ 
+            { 
+                "id": "a", 
+                "cat": "x0"
+            },
+            {
+                "cat": "cp", 
+                "id": "XP_4", 
+                "children": [ 
+                    {
+                        "id": "b", 
+                        "cat": "x0"
+                    }, 
+                    { 
+                        "id": "c", 
+                        "cat": "x0"
+                    } 
+                ]    
+            } 
+        ]
+    };
+    var ptree1 = {
+        "id": 'root',
+        "cat": 'i',
+        "children": [
+            {
+                "id": 'a',
+                "cat": 'w',
+            },
+            {
+                "id": 'cp',
+                "cat": 'phi',
+                "children": [
+                    {
+                        "id": 'b',
+                        "cat": 'w',
+                    },
+                    {
+                        "id": 'c',
+                        "cat": 'w',
+                    }
+                ]
+            }
+        ]
+    }; 
+
+    var ptree2 = {
+        "id": 'root',
+        "cat": 'i',
+        "children": [
+            {
+                "id": 'c',
+                "cat": 'w',
+            },
+            {
+                "id": 'cp',
+                "cat": 'phi',
+                "children": [
+                    {
+                        "id": 'a',
+                        "cat": 'w',
+                    },
+                    {
+                        "id": 'b',
+                        "cat": 'w',
+                    }
+                ]
+            }
+        ]
+    }; 
         
     var stree2 = {
         "id": "CP1",
         "cat": "cp",
-        "children": [ {"cat": "xp", "id": "XP_8", "children": [ {"id": "a", "cat": "x0"} ] },
-        { "cat": "xp", "id": "XP_9", "children": [ {"id": "b", "cat": "x0"} ] },
-        { "cat": "xp", "id": "XP_10", "children": [ {"id": "c", "cat": "x0"} ] } ]
+        "children": [ 
+            {
+                "cat": "xp", 
+                "id": "XP_8", 
+                "children": [ 
+                    {
+                        "id": "a", 
+                        "cat": "x0"
+                    } 
+                ] 
+            },
+            { 
+                "cat": "xp", 
+                "id": "XP_9", 
+                "children": [ 
+                    {
+                        "id": "b", 
+                        "cat": "x0"
+                    } 
+                ] 
+            },
+            {
+                "cat": "xp", 
+                "id": "XP_10", 
+                "children": [ 
+                    {
+                        "id": "c", 
+                        "cat": "x0"
+                    } 
+                ] 
+            } 
+        ]
     };
-        
-       // XPs can be matched by words:
-        matchSP(stree2, ptree1, 'xp', {anyPCat:true}) = 0
-        
-       // Violations still happen when ids don't match:
-       // ptree3 = (x, y, z);
-        matchSP(stree2, ptree3, 'xp', {anyPCat:true}) = 4
+
+    var ptree3 = {
+        "id": 'root',
+        "cat": 'phi',
+        "children": [
+            {
+                "id": 'xp',
+                "cat": 'phi',
+                "children": [
+                    {
+                        "id": 'x',
+                        "cat": 'w'
+                    },
+                    {
+                        "id": 'y',
+                        "cat": 'w'
+                    },
+                    {
+                        "id": 'z',
+                        "cat": 'w'
+                    }
+                ]
+            }
+        ]
+    }; 
+
+    describe("matchAnyPCat.html", function() {
+        describe('Tableau 1: matchSP-cp', function() {
+            it('matchSP-cp', function() {
+                assert.equal(matchSP(stree1, ptree1, 'cp', {anyPCat:true}), 0);
+                assert.equal(matchSP(stree1, ptree2, 'cp', {anyPCat:true}), 1);
+            });
+        });
+        describe('Tableau 2: matchSP-xp', function() {
+            it('matchSP-xp', function() {
+                assert.equal(matchSP(stree2, ptree1, 'xp', {anyPCat:true}), 0);
+                assert.equal(matchSP(stree2, ptree3, 'xp', {anyPCat:true}), 4);
+            });
+        });
+    });
 }
 
 matchAnyPCat();
