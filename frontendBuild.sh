a91652b47e630f36d0b70c110e74d93d79f2d3f6
Su Zin <szin@ucsc.edu>
2021-06-21 10:38:52 -0700

    frontendBuild.sh changes

--- frontendBuild.sh
+++ frontendBuild.sh
@@ -1,10 +1,3 @@
 source testBuild.sh
 cat interface1.html test/frontend_testing/frontend.html > test/frontend_testing/test.html
-sed -i "" 's|build/spot.js|../../build/spot.js|g' test/frontend_testing/test.html
-sed -i "" 's|build/built_in_trees.js|../../build/built_in_trees.js|g' test/frontend_testing/test.html
-sed -i "" 's|lib/test/mocha.min.js|../../lib/test/mocha.min.js|g' test/frontend_testing/test.html
-sed -i "" 's|lib/test/chai.min.js|../../lib/test/chai.min.js|g' test/frontend_testing/test.html
-sed -i "" 's|spot.css|../../spot.css|g' test/frontend_testing/test.html
-sed -i "" 's|lib/test/mocha.css|../../lib/test/mocha.css|g' test/frontend_testing/test.html
-sed -i "" 's|images/favicon.ico|../../images/favicon.ico|g' test/frontend_testing/test.html
-sed -i "" 's|images/logo.png|../../images/logo.png|g' test/frontend_testing/test.html
+sed -i "" 's|src="|src="../../|g;s|" href="|" href="../../|g' test/frontend_testing/test.html
