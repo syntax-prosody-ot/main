32cb3d7ff8f27eed80608de9ff250936279b8530
Su Zin <szin@ucsc.edu>
2021-06-30 14:22:13 -0700

    sed command update

--- frontendBuild.sh
+++ frontendBuild.sh
@@ -1,3 +1,7 @@
 source testBuild.sh
 cat interface1.html test/frontend_testing/frontend.html > test/frontend_testing/test.html
-sed -i "" 's|src="|src="../../|g;s|" href="|" href="../../|g' test/frontend_testing/test.html
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i "" 's|src="|src="../../|g;s|" href="|" href="../../|g' test/frontend_testing/test.html
+else
+  sed -i 's|src="|src="../../|g;s|" href="|" href="../../|g' test/frontend_testing/test.html
+fi
