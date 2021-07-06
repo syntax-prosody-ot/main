fb1ccc5ca1559c090646e8d715838f948daca927
Su Zin <szin@ucsc.edu>
2021-07-05 17:39:27 -0700

    add test/*.js

--- frontendBuild.sh
+++ frontendBuild.sh
@@ -1,7 +1,7 @@
 #source testBuild.sh
 
 source jsbuild.sh
-cat test/frontend_testing/*.js > build/test.js
+cat test/*.js test/frontend_testing/*.js > build/test.js
 cat build/test.js lib/test/*.js >> build/spot.js
 
 cat interface1.html test/frontend_testing/frontend.html > test/frontend_testing/test.html
