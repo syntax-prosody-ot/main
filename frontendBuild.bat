15bf1a36c63e52eb19f476ab8026c73416dcced2
jbellik <jbellik@users.noreply.github.com>
2021-07-22 14:45:19 -0700

    add cautionary comment

--- frontendBuild.bat
+++ frontendBuild.bat
@@ -1,2 +1,4 @@
 call testBuild.bat
 type interface1.html test\frontend_testing\frontend.html 1> test\frontend_testing\test.html 2>NUL
+
+Rem This script doesn't quite work, because it does not do the find-and-replace to fix the relative paths to account for the frontend testing html file being in a different directory.
